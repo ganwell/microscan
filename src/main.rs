ed70a98f10f8bebccb6a9bab44ef6c25a35af37b
Jean-Louis Fuchs <spry.year3230@fastmail.com>
2024-08-27 10:51:02 +0200

    switch to rtic

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,133 @@
 #![no_std]
 #![no_main]
 
-use core::cell::RefCell;
-use core::fmt::{Debug, Write};
-use core::ops::DerefMut;
-use core::panic::PanicInfo;
+use panic_rtt_target as _;
 
-use cortex_m::interrupt::{free as section, Mutex};
-use cortex_m_rt::entry;
-use defmt::println;
-use defmt_rtt as _;
-use embedded_hal::delay::DelayNs;
-use heapless::String;
-use microbit::hal::clocks::Clocks;
-use microbit::pac::{self, interrupt, TIMER0};
-use microbit::Board;
-use rubble::beacon::{Beacon, BeaconScanner, ScanCallback};
-use rubble::link::ad_structure::AdStructure;
-use rubble::link::filter::AllowAll;
-use rubble::link::{CompanyId, DeviceAddress, Metadata, MIN_PDU_BUF};
-use rubble::time::{Duration, Timer as _};
-use rubble_nrf5x::radio::{BleRadio, PacketBuffer};
-use rubble_nrf5x::timer::BleTimer;
-use rubble_nrf5x::utils::get_device_address;
+#[rtic::app(device = microbit::pac, peripherals = true)]
+mod app {
+    use core::mem::MaybeUninit;
 
-pub struct BeaconScanCallback;
+    use microbit::hal::clocks::Clocks;
+    use microbit::pac;
+    use rtt_target::{rprint, rprintln, rtt_init, set_print_channel, ChannelMode};
+    use rubble::beacon::{BeaconScanner, ScanCallback};
+    use rubble::link::ad_structure::AdStructure;
+    use rubble::link::filter::AllowAll;
+    use rubble::link::{DeviceAddress, Metadata, MIN_PDU_BUF};
+    use rubble::time::{Duration, Timer};
+    use rubble_nrf5x::radio::{BleRadio, PacketBuffer};
+    use rubble_nrf5x::timer::BleTimer;
 
-impl ScanCallback for BeaconScanCallback {
-    fn beacon<'a, I>(&mut self, addr: DeviceAddress, data: I, metadata: Metadata)
-    where
-        I: Iterator<Item = AdStructure<'a>>,
-    {
-        let raw = addr.raw();
-        if raw[0] == 101 {
+    pub struct BeaconScanCallback;
+
+    impl ScanCallback for BeaconScanCallback {
+        fn beacon<'a, I>(&mut self, addr: DeviceAddress, data: I, metadata: Metadata)
+        where
+            I: Iterator<Item = AdStructure<'a>>,
+        {
+            rprint!(
+                "[{:?}] CH:{:?} Type:{:?} ",
+                metadata.timestamp.unwrap().ticks(),
+                metadata.channel,
+                metadata.pdu_type.unwrap(),
+            );
             if let Some(rssi) = metadata.rssi {
-                println!("RSSI:{:?}dBm ", rssi);
+                rprint!("RSSI:{:?}dBm ", rssi);
             }
+            rprint!("BDADDR:{:?} DATA:", addr);
+            let mut first = true;
+            for packet in data {
+                rprint!("{}{:02x?}", if first { " " } else { " / " }, packet);
+                first = false;
+            }
+            rprintln!("");
+            rprintln!("");
         }
     }
-}
 
-struct Shared {
-    radio: BleRadio,
-    timer: BleTimer<TIMER0>,
-    scanner: BeaconScanner<BeaconScanCallback, AllowAll>,
-}
-
-static SHARED: Mutex<RefCell<Option<Shared>>> = Mutex::new(RefCell::new(None));
-
-fn hdebug<T: Debug>(str: T) -> &'static str {
-    static mut S: String<512> = String::new();
-    unsafe {
-        S.clear();
-        write!(S, "{:?}", str);
-        S.as_str()
+    #[shared]
+    struct Shared {
+        radio: BleRadio,
+        ble_timer: BleTimer<pac::TIMER0>,
+        scanner: BeaconScanner<BeaconScanCallback, AllowAll>,
     }
-}
 
-#[entry]
-fn main() -> ! {
-    static mut BLE_TX_BUF: PacketBuffer = [0; MIN_PDU_BUF];
-    static mut BLE_RX_BUF: PacketBuffer = [0; MIN_PDU_BUF];
-    if let Some(p) = microbit::Peripherals::take() {
-        let _clocks = Clocks::new(p.CLOCK).enable_ext_hfosc();
+    #[local]
+    struct Local {}
 
-        let mut timer = BleTimer::init(p.TIMER0);
+    #[init(local=[
+        tx_buf: MaybeUninit<PacketBuffer> = MaybeUninit::uninit(),
+        rx_buf: MaybeUninit<PacketBuffer> = MaybeUninit::uninit()
+    ])]
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        let rtt = rtt_init! {
+            up: {
+                0: {
+                    size: 1024,
+                    mode: ChannelMode::NoBlockTrim,
+                    name: "Microscan Logs"
+                }
+            }
+        };
+        set_print_channel(rtt.up.0);
 
-        let mut radio = BleRadio::new(p.RADIO, &p.FICR, BLE_TX_BUF, BLE_RX_BUF);
+        let _clocks = Clocks::new(ctx.device.CLOCK).enable_ext_hfosc();
+
+        let mut ble_timer = BleTimer::init(ctx.device.TIMER0);
+
+        let ble_rx_buf: &'static mut _ = ctx.local.rx_buf.write([0; MIN_PDU_BUF]);
+        let ble_tx_buf: &'static mut _ = ctx.local.tx_buf.write([0; MIN_PDU_BUF]);
+        let mut radio =
+            BleRadio::new(ctx.device.RADIO, &ctx.device.FICR, ble_tx_buf, ble_rx_buf);
 
         let mut scanner = BeaconScanner::new(BeaconScanCallback);
-        let scanner_cmd = scanner.configure(timer.now(), Duration::millis(500));
+        let scanner_cmd = scanner.configure(ble_timer.now(), Duration::millis(500));
 
         radio.configure_receiver(scanner_cmd.radio);
-        timer.configure_interrupt(scanner_cmd.next_update);
+        ble_timer.configure_interrupt(scanner_cmd.next_update);
 
-        let shared = Shared {
-            radio,
-            timer,
-            scanner,
-        };
-        section(|cs| SHARED.borrow(cs).replace(Some(shared)));
-        unsafe {
-            pac::NVIC::unmask(pac::Interrupt::TIMER0);
-            pac::NVIC::unmask(pac::Interrupt::RADIO);
-        }
-        println!("Scanner set up");
-        loop {}
+        rprintln!("nRF52 scanner ready!");
+
+        (
+            Shared {
+                radio,
+                scanner,
+                ble_timer,
+            },
+            Local {},
+            init::Monotonics(),
+        )
     }
-    panic!("End");
-}
 
-#[interrupt]
-fn RADIO() {
-    section(|cs| {
-        if let Some(ref mut shared) = SHARED.borrow(cs).borrow_mut().deref_mut() {
-            let timer = &mut shared.timer;
-            let radio = &mut shared.radio;
-            let scanner = &mut shared.scanner;
+    #[task(binds = RADIO, shared = [radio, scanner, ble_timer])]
+    fn radio(ctx: radio::Context) {
+        let timer = ctx.shared.ble_timer;
+        let scanner = ctx.shared.scanner;
+        let radio = ctx.shared.radio;
+
+        (timer, scanner, radio).lock(|timer, scanner, radio| {
             if let Some(next_update) = radio.recv_beacon_interrupt(timer.now(), scanner)
             {
                 timer.configure_interrupt(next_update);
             }
-        }
-    });
-}
+        });
+    }
 
-#[interrupt]
-fn TIMER0() {
-    section(|cs| {
-        if let Some(ref mut shared) = SHARED.borrow(cs).borrow_mut().deref_mut() {
-            let timer = &mut shared.timer;
-            let radio = &mut shared.radio;
-            let scanner = &mut shared.scanner;
+    #[task(binds = TIMER0, shared = [radio, ble_timer, scanner])]
+    fn timer0(ctx: timer0::Context) {
+        let timer = ctx.shared.ble_timer;
+        let scanner = ctx.shared.scanner;
+        let radio = ctx.shared.radio;
+
+        (timer, scanner, radio).lock(|timer, scanner, radio| {
             if !timer.is_interrupt_pending() {
                 return;
             }
             timer.clear_interrupt();
+
             let cmd = scanner.timer_update(timer.now());
             radio.configure_receiver(cmd.radio);
             timer.configure_interrupt(cmd.next_update);
-        }
-    });
-}
-
-#[inline(never)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    let mut s: String<1024> = String::new();
-    write!(s, "Panic: {:?}", info).unwrap();
-    println!("{}", s.as_str());
-    loop {}
+        });
+    }
 }
